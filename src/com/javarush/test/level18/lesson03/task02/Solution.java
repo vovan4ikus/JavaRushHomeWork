7b5d990f58b81f9c83c24e0a020d4c74aaa59454
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-10-05 15:48:13 +0300

    added lines

--- src/com/javarush/test/level18/lesson03/task02/Solution.java
+++ src/com/javarush/test/level18/lesson03/task02/Solution.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 Закрыть поток ввода-вывода
 */
 
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
