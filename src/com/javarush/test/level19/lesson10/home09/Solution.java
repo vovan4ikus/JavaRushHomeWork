9b3e248b6894d1b15efe83f8a807d860ddef2ad9
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-10-29 12:57:39 +0300

    some

---
+++ src/com/javarush/test/level19/lesson10/home09/Solution.java
@@ -0,0 +1,74 @@
+package com.javarush.test.level19.lesson10.home09;
+
+/* Контекстная реклама
+В методе main подмените объект System.out написанной вами реадер-оберткой
+Ваша реадер-обертка должна выводить на консоль контекстную рекламу после каждого второго println-а
+Вызовите готовый метод printSomething(), воспользуйтесь testString
+Верните переменной System.out первоначальный поток
+
+Рекламный текст: "JavaRush - курсы Java онлайн"
+
+Пример вывода:
+first
+second
+JavaRush - курсы Java онлайн
+third
+fourth
+JavaRush - курсы Java онлайн
+fifth
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        //запоминаем настоящий PrintStream в специальную переменную
+        PrintStream consoleStream = System.out;
+
+        //Создаем динамический массив
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //создаем адаптер к классу PrintStream
+        PrintStream stream = new PrintStream(outputStream);
+        //Устанавливаем его как текущий System.out
+        System.setOut(stream);
+
+        //Вызываем функцию, которая ничего не знает о наших манипуляциях
+        testString.printSomething();
+
+        //Преобразовываем записанные в наш ByteArray данные в строку
+        String result = outputStream.toString();
+
+        //Возвращаем все как было
+        System.setOut(consoleStream);
+
+        //разворачиваем строку
+        int total = 0;
+        String[] str = result.split(" ");
+        if (str[1].equals("+")) {
+            total = Integer.parseInt(str[0]) + Integer.parseInt(str[2]);
+        }
+        if (str[1].equals("*")) {
+            total = Integer.parseInt(str[0]) * Integer.parseInt(str[2]);
+        }
+        if (str[1].equals("-")) {
+            total = Integer.parseInt(str[0]) - Integer.parseInt(str[2]);
+        }
+        //выводим ее в консоль
+        System.out.println(str[0] + " " + str[1] + " " + str[2] + " " + str[3] + " " + total);
+
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("first");
+            System.out.println("second");
+            System.out.println("third");
+            System.out.println("fourth");
+            System.out.println("fifth");
+        }
+    }
+}
