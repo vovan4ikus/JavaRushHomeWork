881dba654dfdf3256db0990d1e54227ced518094
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-11-01 15:23:14 +0200

    done

--- src/com/javarush/test/level20/lesson07/task03/Solution.java
+++ src/com/javarush/test/level20/lesson07/task03/Solution.java
@@ -17,9 +17,11 @@ public class Solution {
         private String firstName;
         private String lastName;
         private int age;
-        private Person mother;
-        private Person father;
-        private List<Person> children;
+        transient private Person mother;
+        transient private Person father;
+        transient private List<Person> children;
+
+        public Person(){}
 
         public Person(String firstName, String lastName, int age) {
             this.firstName = firstName;
@@ -43,20 +45,20 @@ public class Solution {
         public void writeExternal(ObjectOutput out) throws IOException {
             out.writeObject(mother);
             out.writeObject(father);
-            out.writeChars(firstName);
-            out.writeChars(lastName);
+            out.writeObject(firstName);
+            out.writeObject(lastName);
             out.writeInt(age);
             out.writeObject(children);
         }
 
         @Override
         public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-            firstName = in.readLine();
-            lastName = in.readLine();
-            father = (Person)in.readObject();
             mother = (Person)in.readObject();
+            father = (Person)in.readObject();
+            firstName = (String)in.readObject();
+            lastName = (String)in.readObject();
             age = in.readInt();
-            children = (List)in.readObject();
+            children = (List<Person>)in.readObject();
         }
     }
 }
