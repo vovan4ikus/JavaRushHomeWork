1680794e6db7cce39fde1b1c9cbf142ed63a2dc7
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-11-07 11:57:09 +0200

    done

--- src/com/javarush/test/level20/lesson10/home03/Solution.java
+++ src/com/javarush/test/level20/lesson10/home03/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.test.level20.lesson10.home03;
 
-import java.io.Serializable;
+import java.io.*;
 
 /* Найти ошибки
 Почему-то при сериализации/десериализации объекта класса B возникают ошибки.
@@ -10,9 +10,30 @@ import java.io.Serializable;
 Метод main не участвует в тестировании.
 */
 public class Solution {
+//    public static void main(String[] args) throws IOException, ClassNotFoundException {
+//        FileOutputStream fileOutput = new FileOutputStream("your.file.name");
+//        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutput);
+//
+//        B b = new B("B");
+//        outputStream.writeObject(b);
+//        fileOutput.flush();
+//        fileOutput.close();
+//        outputStream.close();
+//
+//        FileInputStream fiStream = new FileInputStream("your.file.name");
+//        ObjectInputStream objectStream = new ObjectInputStream(fiStream);
+//
+//        B b1 = (B) objectStream.readObject();
+//
+//        fiStream.close();
+//        objectStream.close();
+//
+//
+//    }
+
     public static class A {
         protected String name = "A";
-
+        public A(){}
         public A(String name) {
             this.name += name;
         }
@@ -22,6 +43,12 @@ public class Solution {
         public B(String name) {
             super(name);
             this.name += name;
+        }
+        private void writeObject(ObjectOutputStream out) throws IOException {
+            out.writeObject(name);
+        }
+        private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+            name = (String)in.readObject();
         }
     }
 }
