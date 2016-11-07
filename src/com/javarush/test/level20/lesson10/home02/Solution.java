81705a2278a105a7ad964fe42bf117a9f20ce018
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-11-07 11:00:33 +0200

    done

--- src/com/javarush/test/level20/lesson10/home02/Solution.java
+++ src/com/javarush/test/level20/lesson10/home02/Solution.java
@@ -1,19 +1,25 @@
 package com.javarush.test.level20.lesson10.home02;
 
+import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.Serializable;
 
 /* Десериализация
 На вход подается поток, в который записан сериализованный объект класса A либо класса B.
 Десериализуйте объект в методе getOriginalObject предварительно определив, какого именно типа там объект.
 Реализуйте интерфейс Serializable где необходимо.
 */
-public class Solution {
-    public A getOriginalObject(ObjectInputStream objectStream) {
+public class Solution implements Serializable {
+    public A getOriginalObject(ObjectInputStream objectStream) throws IOException, ClassNotFoundException {
+        Object o  =  objectStream.readObject();
+        if (o instanceof A) return (A) o;
+        else return (B) o;
 
-        return null;
+
+//        return null;
     }
 
-    public class A {
+    public class A implements Serializable{
     }
 
     public class B extends A {
