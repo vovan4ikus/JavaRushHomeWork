463953ad2cb1435e2ccae68904b98a5fd56f7f84
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-11-08 15:24:19 +0200

    done...

--- src/com/javarush/test/level20/lesson10/home09/Solution.java
+++ src/com/javarush/test/level20/lesson10/home09/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.test.level20.lesson10.home09;
 
+import java.io.*;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,7 +11,36 @@ import java.util.List;
 Сериализовать Solution.
 Все данные должны сохранить порядок следования.
 */
-public class Solution {
+public class Solution implements Serializable {
     int node;
     List<Solution> edges = new LinkedList<>();
+    public static void main(String[] args) throws Exception {
+        Solution solution1 = new Solution();
+        solution1.node = 1;
+        Solution solution2 = new Solution();
+        solution2.node = 2;
+        Solution solution3 = new Solution();
+        solution3.node = 3;
+        solution1.edges.add(solution2);
+        solution1.edges.add(solution3);
+        solution1.edges.add(solution2);
+        solution1.edges.add(solution1);
+
+        String fileName = "1.txt";
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fileName));
+        oos.writeObject(solution1);
+        oos.close();
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+        Solution loadedS = (Solution)ois.readObject();
+        ois.close();
+
+        System.out.println("Original:");
+        System.out.println(solution1.node);
+        for (Solution s : solution1.edges) System.out.print(s.node + " ");
+
+        System.out.println("\nLoaded:");
+        System.out.println(loadedS.node);
+        for (Solution s : loadedS.edges) System.out.print(s.node + " ");
+    }
 }
