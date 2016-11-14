e257c3b833cfb0a94f5539f1830328180f0aace5
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-11-14 09:51:34 +0200

    done

---
+++ src/com/javarush/test/level21/lesson02/task03/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level21.lesson02.task03;
+
+/* Все гениальное - просто!
+Упростить. Переменные не переименовывать, комментарии не оставлять.
+*/
+public class Solution {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+//        return (a && b && c && !d) || (!a && c) || (!b && c) || (c && d);
+//        return (a && b && !d || !a || !b || d) && c;
+        return c;
+    }
+
+}
