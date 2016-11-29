2277d9cdf902f4aa66ef70d38832e0e8deb93e6d
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-11-29 22:05:13 +0200

    done2*

--- src/com/javarush/test/level22/lesson09/task03/Solution.java
+++ src/com/javarush/test/level22/lesson09/task03/Solution.java
@@ -25,49 +25,48 @@ import java.util.Scanner;
 Амстердам Мельбурн Нью-Йорк Киев Вена
 */
 public class Solution {
-    public static void main(String[] args) throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         //...
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Scanner scanner = new Scanner(new FileReader(reader.readLine()));
         String www = "";
-        while (scanner.hasNextLine())
-        {
+        while (scanner.hasNextLine()) {
             www = scanner.nextLine();
         }
         StringBuilder result = getLine(www.split(" "));
         System.out.println(result.toString());
     }
+
     public static StringBuilder getLine(String... words) {
-        if (words == null || words.length == 0)
-        { return new StringBuilder(); }
-        if ("".equals(words[0]) || words.length == 1)
-        {return new StringBuilder(words[0]); }
+        if (words == null || words.length == 0) {
+            return new StringBuilder();
+        }
+        if ("".equals(words[0]) || words.length == 1) {
+            return new StringBuilder(words[0]);
+        }
 
         StringBuilder result = new StringBuilder();
 
         ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < words.length; i ++)
+        for (int i = 0; i < words.length; i++)
             if (!words[i].equals(""))
                 list.add(words[i]);
-        while (!isOK(list))
-        {
+        while (!isOK(list)) {
             Collections.shuffle(list);
         }
         for (String s : list)
-            result.append(s+ " ");
-        result.deleteCharAt(result.length()-1);
+            result.append(s + " ");
+        result.deleteCharAt(result.length() - 1);
         return result;
     }
-    public static boolean isOK(ArrayList<String> list)
-    {
-        for (int i = 0; i < list.size()-1; i++)
-        {
+
+    public static boolean isOK(ArrayList<String> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
             String first = list.get(i);
-            String second = list.get(i+1);
+            String second = list.get(i + 1);
             first = first.toLowerCase();
-            second =second.toLowerCase();
-            if (first.charAt(first.length()-1)!=second.charAt(0))
+            second = second.toLowerCase();
+            if (first.charAt(first.length() - 1) != second.charAt(0))
                 return false;
         }
         return true;
