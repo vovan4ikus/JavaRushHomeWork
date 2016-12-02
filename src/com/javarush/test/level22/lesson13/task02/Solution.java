4dbf83e5301af8c55c2f46d37510454b24eaa6c2
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-02 16:29:26 +0200

    done

--- src/com/javarush/test/level22/lesson13/task02/Solution.java
+++ src/com/javarush/test/level22/lesson13/task02/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.test.level22.lesson13.task02;
 
-import java.io.IOException;
+import java.io.*;
 
 /* Смена кодировки
 В метод main первым параметром приходит имя файла, тело которого в кодировке Windows-1251.
@@ -10,5 +10,17 @@ public class Solution {
     static String win1251TestString = "РќР°СЂСѓС€РµРЅРёРµ РєРѕРґРёСЂРѕРІРєРё РєРѕРЅСЃРѕР»Рё?"; //only for your testing
 
     public static void main(String[] args) throws IOException {
+        FileInputStream input = new FileInputStream(args[0]);
+        FileOutputStream out = new FileOutputStream(args[1]);
+
+        byte[] buffer = new byte[input.available()];
+        input.read(buffer);
+        String s = new String(buffer, "UTF-8");
+
+        buffer = s.getBytes("Windows-1251");
+        out.write(buffer);
+
+        input.close();
+        out.close();
     }
 }
