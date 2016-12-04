7cfbac314bbb3a1e4549807fab783c7c46096ff5
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-04 19:11:05 +0200

    3

--- src/com/javarush/test/level22/lesson18/big01/Tetris.java
+++ src/com/javarush/test/level22/lesson18/big01/Tetris.java
@@ -4,6 +4,17 @@ package com.javarush.test.level22.lesson18.big01;
  * Created by User on 04.12.16.
  */
 public class Tetris {
+    private Field field;
+    private Figure figure;
+
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     public static void main(String[] args) {
 
     }
