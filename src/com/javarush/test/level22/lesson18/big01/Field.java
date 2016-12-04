8f141d4081939a69ddbbb9f5506c3ca720e1f887
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-04 19:22:17 +0200

    6

--- src/com/javarush/test/level22/lesson18/big01/Field.java
+++ src/com/javarush/test/level22/lesson18/big01/Field.java
@@ -4,4 +4,24 @@ package com.javarush.test.level22.lesson18.big01;
  * Created by User on 04.12.16.
  */
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return new int[height][width];
+    }
+
+    public Field(int width, int height){
+        this.height = height;
+        this.width = width;
+    }
 }
