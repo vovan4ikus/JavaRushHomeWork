81df29838812fe7ff8f8d02a0ca80d4b4f712494
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-12 16:52:28 +0200

    task 8

--- src/com/javarush/test/level23/lesson13/big01/Snake.java
+++ src/com/javarush/test/level23/lesson13/big01/Snake.java
@@ -6,9 +6,25 @@ import java.util.ArrayList;
  * Created by User on 12.12.16.
  */
 public class Snake {
-    ArrayList<SnakeSection> sections = new ArrayList<>();
+    ArrayList<SnakeSection> sections;
     boolean isAlive;
     SnakeDirection direction;
+
+    public Snake(int x, int y){
+        sections = new ArrayList<>();
+        sections.add(0,new SnakeSection(x,y));
+        isAlive = true;
+    }
+
+    public int getX(){
+        return sections.get(0).getX();
+    }
+
+    public int getY(){
+        return sections.get(0).getY();
+    }
+
+    public void move(){}
 
     public ArrayList<SnakeSection> getSections() {
         return sections;
