5d5a58a62364107ee45b1044efc4d410a0b444ce
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-12 16:37:37 +0200

    task 5

--- src/com/javarush/test/level23/lesson13/big01/SnakeDirection.java
+++ src/com/javarush/test/level23/lesson13/big01/SnakeDirection.java
@@ -3,5 +3,9 @@ package com.javarush.test.level23.lesson13.big01;
 /**
  * Created by User on 12.12.16.
  */
-public class SnakeDirection {
+public enum SnakeDirection {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
 }
