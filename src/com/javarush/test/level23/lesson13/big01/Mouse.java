24b5203851ed87c67ede44580c4e66c0310f68f2
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-12 16:36:20 +0200

    task 4

--- src/com/javarush/test/level23/lesson13/big01/Mouse.java
+++ src/com/javarush/test/level23/lesson13/big01/Mouse.java
@@ -4,4 +4,19 @@ package com.javarush.test.level23.lesson13.big01;
  * Created by User on 12.12.16.
  */
 public class Mouse {
+    int x;
+    int y;
+
+    public Mouse(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
