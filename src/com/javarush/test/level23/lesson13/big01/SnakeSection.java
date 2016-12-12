3e4a598e531a81fddbc09e616edcb58dbebb2207
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-12 16:39:16 +0200

    task 6

--- src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
+++ src/com/javarush/test/level23/lesson13/big01/SnakeSection.java
@@ -4,4 +4,19 @@ package com.javarush.test.level23.lesson13.big01;
  * Created by User on 12.12.16.
  */
 public class SnakeSection {
+    int x;
+    int y;
+
+    public SnakeSection(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
