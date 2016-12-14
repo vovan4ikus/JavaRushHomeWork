82a53cd475579158f8f59e158498d90a105f6a9f
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-14 12:45:24 +0200

    done

--- src/com/javarush/test/level24/lesson04/home01/Solution.java
+++ src/com/javarush/test/level24/lesson04/home01/Solution.java
@@ -1,25 +1,20 @@
 package com.javarush.test.level24.lesson04.home01;
 
-import com.javarush.test.level24.lesson04.home01.HasHeight;
-import com.javarush.test.level24.lesson04.home01.HasWidth;
-import com.javarush.test.level24.lesson04.home01.Point;
-
 /* Рефакторинг
 В классе Rectangle:
-1. Измените методы getHeight и getWidth, чтобы они возвращали HasHeight и HasWidth соответственно.
-2. Для этого внутри методов getHeight и getWidth создайте локальные классы - реализации интерфейсов.
-3. Переименуйте getHeight в castToHasHeight, getWidth в castToHasWidth (на имени метода нажмите Shift+F6).
+1. Измените методы castToHasHeight и getWidth, чтобы они возвращали HasHeight и castToHasWidth соответственно.
+2. Для этого внутри методов castToHasHeight и getWidth создайте локальные классы - реализации интерфейсов.
+3. Переименуйте castToHasHeight в castToHasHeight, getWidth в castToHasWidth (на имени метода нажмите Shift+F6).
 4. Уберите наследование интерфейсов в классе  Rectangle.
 Методы интерфейсов не менять.
 */
 public class Solution {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(1, 2, 3, 4);
-        System.out.println(getHeight(rectangle));
-        System.out.println(getWidth(rectangle));
-        /////////////////////expected//////////////////
-        //System.out.println(getHeight(rectangle.castToHasHeight()));
-        //System.out.println(getWidth(rectangle.castToHasWidth()));
+//        System.out.println(getHeight(rectangle));
+//        System.out.println(getWidth(rectangle));
+        System.out.println(getHeight(rectangle.castToHasHeight()));
+        System.out.println(getWidth(rectangle.castToHasWidth()));
     }
 
     public static double getHeight(HasHeight rectangle) {
@@ -31,7 +26,7 @@ public class Solution {
     }
 
 
-    public static class Rectangle implements HasHeight, HasWidth{
+    public static class Rectangle {
         private Point point1;
         private Point point2;
 
@@ -40,12 +35,24 @@ public class Solution {
             point2 = new Point(x2, y2);
         }
 
-        public double getHeight() {
-            return Math.abs(point1.getY() - point2.getY());
+        public HasHeight castToHasHeight() {
+            return new HasHeight() {
+                @Override
+                public double getHeight()
+                {
+                    return Math.abs(point1.getY() - point2.getY());
+                }
+            };
         }
 
-        public double getWidth() {
-            return Math.abs(point1.getX() - point2.getX());
+        public HasWidth castToHasWidth() {
+            return new HasWidth() {
+                @Override
+                public double getWidth()
+                {
+                    return Math.abs(point1.getX() - point2.getX());
+                }
+            };
         }
     }
 }
