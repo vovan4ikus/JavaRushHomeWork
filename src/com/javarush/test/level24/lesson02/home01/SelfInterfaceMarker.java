18e15565775729c3d4d9fcd26318c796b4670ee6
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-14 11:45:11 +0200

    done

---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by Володимир on 14.12.2016.
+ */
+public interface SelfInterfaceMarker {
+}
---
+++ src/com/javarush/test/level24/lesson02/home01/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level24.lesson02.home01;
+
+/**
+ * Created by Володимир on 14.12.2016.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public SelfInterfaceMarkerImpl() {
+    }
+    public void print1(){
+        System.out.println("print1");
+    }
+    public void print2(){
+        System.out.println("print2");
+    }
+}
