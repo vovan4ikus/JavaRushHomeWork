e0c0a55fd126de7a1360dc3ea5f8ae7057594a91
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-18 17:46:34 +0200

    4

--- src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
+++ src/com/javarush/test/level24/lesson14/big01/Arcanoid.java
@@ -1,11 +1,40 @@
 package com.javarush.test.level24.lesson14.big01;
 
+import java.util.ArrayList;
+
 /**
  * Created by User on 18.12.16.
  */
 public class Arcanoid {
     int width;
     int height;
+    Ball ball;
+    Stand stand;
+    ArrayList<Brick> bricks;
+
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public ArrayList<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(ArrayList<Brick> bricks) {
+        this.bricks = bricks;
+    }
 
     public Arcanoid(int width, int height) {
         this.width = width;
