9e049cf6fa1d664785f853affd765e1f4cac0d9a
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-18 17:53:13 +0200

    6

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -3,5 +3,5 @@ package com.javarush.test.level24.lesson14.big01;
 /**
  * Created by User on 18.12.16.
  */
-public class Ball {
+public class Ball extends BaseObject {
 }
--- src/com/javarush/test/level24/lesson14/big01/Brick.java
+++ src/com/javarush/test/level24/lesson14/big01/Brick.java
@@ -3,5 +3,5 @@ package com.javarush.test.level24.lesson14.big01;
 /**
  * Created by User on 18.12.16.
  */
-public class Brick {
+public class Brick extends BaseObject {
 }
