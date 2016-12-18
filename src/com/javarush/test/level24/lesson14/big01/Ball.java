ffe49e3dfb092df63aa2ee86ff72e712a675e0f3
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-18 17:56:28 +0200

    7

--- src/com/javarush/test/level24/lesson14/big01/Ball.java
+++ src/com/javarush/test/level24/lesson14/big01/Ball.java
@@ -4,4 +4,7 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by User on 18.12.16.
  */
 public class Ball extends BaseObject {
+    public Ball(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
--- src/com/javarush/test/level24/lesson14/big01/BaseObject.java
+++ src/com/javarush/test/level24/lesson14/big01/BaseObject.java
@@ -4,4 +4,37 @@ package com.javarush.test.level24.lesson14.big01;
  * Created by User on 18.12.16.
  */
 public class BaseObject {
+    double x;
+    double y;
+    double radius;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
