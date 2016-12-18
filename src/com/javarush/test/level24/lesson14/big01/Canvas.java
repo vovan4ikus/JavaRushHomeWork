cea9d6a19fabac28d40f7dc9e16e68c34f82b03d
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-18 19:39:21 +0200

    11

--- src/com/javarush/test/level24/lesson14/big01/Canvas.java
+++ src/com/javarush/test/level24/lesson14/big01/Canvas.java
@@ -56,4 +56,27 @@ public class Canvas {
 
         matrix[y0][x0] = c;
     }
+    public void print()
+    {
+        for (int i = 0; i < matrix.length; i++)
+        {
+            for (int j = 0; j < matrix[i].length ; j++)
+            {
+                System.out.print(matrix[i][j]);
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+    }
+    public void clear()
+    {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = ' ';
+            }
+        }
+    }
 }
