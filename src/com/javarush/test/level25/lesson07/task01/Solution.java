3ddfc04d284288478dc753a34b80ef3366e132c3
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-23 19:44:06 +0200

    done

--- src/com/javarush/test/level25/lesson07/task01/Solution.java
+++ src/com/javarush/test/level25/lesson07/task01/Solution.java
@@ -20,6 +20,15 @@ public class Solution extends Thread {
 
     public void interrupt() {
         //implement logic here
+
+        try {
+            socket.close();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            super.interrupt();
+        }
     }
 
     public void run() {
