9407ef13e4495f0bcdba58c9d59ecba352e430c6
Ustimenko Vladimir <ustimmmenko@gmail.com>
2016-12-30 13:21:16 +0200

    8

--- src/com/javarush/test/level25/lesson16/big01/Canvas.java
+++ src/com/javarush/test/level25/lesson16/big01/Canvas.java
@@ -4,4 +4,25 @@ package com.javarush.test.level25.lesson16.big01;
  * Created by User on 30.12.16.
  */
 public class Canvas {
+    private int width;
+    private int height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.height = height;
+        this.width = width;
+        this.matrix = new char[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public char[][] getMatrix() {
+        return matrix;
+    }
 }
