831afcaad3ae5a196809cccda855757215c30815
Ustimenko Vladimir <ustimmmenko@gmail.com>
2017-01-05 16:28:45 +0200

    done

--- src/com/javarush/test/level26/lesson02/task01/Solution.java
+++ src/com/javarush/test/level26/lesson02/task01/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level26.lesson02.task01;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /* Почитать в инете про медиану выборки
 Реализовать логику метода sort, который должен сортировать данные в массиве по удаленности от его медианы
 Вернуть отсортированный массив от минимального расстояния до максимального
@@ -8,6 +11,25 @@ package com.javarush.test.level26.lesson02.task01;
 public class Solution {
     public static Integer[] sort(Integer[] array) {
         //implement logic here
+        Arrays.sort(array);
+        final double median;
+        if (array.length % 2 == 0)
+            median = ((double)array[array.length/2-1]+(double)array[array.length/2])/2;
+        else
+            median = array[array.length/2];
+        Comparator<Integer> compareByMedian = new Comparator<Integer>()
+        {
+            @Override
+            public int compare(Integer o1, Integer o2)
+            {
+                double value = Math.abs(o1-median) - Math.abs(o2-median);
+                if (value == 0)
+                    value = o1 - o2;
+                return (int)value;
+            }
+        };
+        Arrays.sort(array, compareByMedian);
+        System.out.println(median);
         return array;
     }
 }
