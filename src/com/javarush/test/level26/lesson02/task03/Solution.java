fde26f1e1e6b706c2bba70bf13f71df90eb8182a
Ustimenko Vladimir <ustimmmenko@gmail.com>
2017-01-05 17:08:37 +0200

    done

--- src/com/javarush/test/level26/lesson02/task03/Solution.java
+++ src/com/javarush/test/level26/lesson02/task03/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level26.lesson02.task03;
 
+import java.util.Comparator;
+
 /* Убежденному убеждать других не трудно.
 В таблице есть колонки, по которым можно сортировать.
 Пользователь имеет возможность настроить под себя список колонок, которые будут сортироваться.
@@ -10,4 +12,25 @@ package com.javarush.test.level26.lesson02.task03;
 В конструктор передается как минимум один компаратор
 */
 public class Solution {
+    public static class CustomizedComparator<T> implements Comparator<T> {
+        private Comparator<T>[] comparators;
+
+        public CustomizedComparator(Comparator<T>... comparators)
+        {
+            this.comparators = comparators;
+        }
+
+        @Override
+        public int compare(T o1, T o2)
+        {
+            int difference = 0;
+            for (Comparator<T> comparator : comparators)
+            {
+                difference = comparator.compare(o1, o2);
+                if (difference != 0)
+                    break;
+            }
+            return difference;
+        }
+    }
 }
