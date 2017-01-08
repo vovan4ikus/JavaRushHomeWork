904f0113517fa45aee9abd53322495202cf5647e
Ustimenko Vladimir <ustimmmenko@gmail.com>
2017-01-08 09:27:40 +0200

    1

---
+++ src/com/javarush/test/level26/lesson15/big01/CashMachine.java
@@ -0,0 +1,10 @@
+package com.javarush.test.level26.lesson15.big01;
+
+/**
+ * Created by User on 08.01.17.
+ */
+public class CashMachine {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/javarush/test/level26/lesson15/big01/Operation.java
@@ -0,0 +1,8 @@
+package com.javarush.test.level26.lesson15.big01;
+
+/**
+ * Created by User on 08.01.17.
+ */
+public enum Operation {
+    INFO, DEPOSIT, WITHDRAW, EXIT
+}
