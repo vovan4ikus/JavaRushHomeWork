62faf8eb359e311b36589ac3c3877d107fe2db7d
Ustimenko Vladimir <ustimmmenko@gmail.com>
2017-01-08 09:39:57 +0200

    2

--- src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
+++ src/com/javarush/test/level26/lesson15/big01/ConsoleHelper.java
@@ -1,7 +1,27 @@
 package com.javarush.test.level26.lesson15.big01;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by User on 08.01.17.
  */
 public class ConsoleHelper {
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+    public static String readString(){
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = null;
+        try {
+            s = reader.readLine();
+        } catch (Exception e) {}
+        finally{
+            try {
+                reader.close();
+            } catch (IOException e) {}
+        }
+        return s;
+    }
 }
---
+++ src/com/javarush/test/level26/lesson15/big01/exception/NotEnoughMoneyException.java
@@ -0,0 +1,7 @@
+package com.javarush.test.level26.lesson15.big01.exception;
+
+/**
+ * Created by User on 08.01.17.
+ */
+public class NotEnoughMoneyException extends Exception {
+}
