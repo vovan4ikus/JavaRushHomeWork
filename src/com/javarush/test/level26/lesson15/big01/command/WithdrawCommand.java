1a357a5dc48662b5bcfa433dcfad98f655137fc3
Ustimenko Vladimir <ustimmmenko@gmail.com>
2017-01-10 19:05:58 +0200

    10

--- src/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
+++ src/com/javarush/test/level26/lesson15/big01/command/WithdrawCommand.java
@@ -1,13 +1,47 @@
 package com.javarush.test.level26.lesson15.big01.command;
 
+import com.javarush.test.level26.lesson15.big01.ConsoleHelper;
+import com.javarush.test.level26.lesson15.big01.CurrencyManipulator;
+import com.javarush.test.level26.lesson15.big01.CurrencyManipulatorFactory;
 import com.javarush.test.level26.lesson15.big01.exception.InterruptOperationException;
+import com.javarush.test.level26.lesson15.big01.exception.NotEnoughMoneyException;
 
 /**
  * Created by User on 10.01.17.
  */
 class WithdrawCommand implements Command {
     @Override
-    public void execute() {
+    public void execute() throws InterruptOperationException {
+        ConsoleHelper.writeMessage("Enter currency code");
+        String currencyCode = ConsoleHelper.askCurrencyCode();
+        CurrencyManipulator currencyManipulator = CurrencyManipulatorFactory.getManipulatorByCurrencyCode(currencyCode);
+        int sum;
+        while (true) {
+            ConsoleHelper.writeMessage("before");
+            String s = ConsoleHelper.readString();
+            try {
+                sum = Integer.parseInt(s);
+            } catch (NumberFormatException e) {
+                ConsoleHelper.writeMessage("specify.amount");
+                continue;
+            }
+            if (sum <= 0) {
+                ConsoleHelper.writeMessage("specify.not.empty.amount");
+                continue;
+            }
+            if (!currencyManipulator.isAmountAvailable(sum)) {
+                ConsoleHelper.writeMessage("not.enough.money");
+                continue;
+            }
+            try {
+                currencyManipulator.withdrawAmount(sum);
+            } catch (NotEnoughMoneyException e) {
+                ConsoleHelper.writeMessage("exact.amount.not.available");
+                continue;
+            }
+            ConsoleHelper.writeMessage(String.format("success.format", sum, currencyCode));
+            break;
 
+        }
     }
 }
